42ae614cbcd6d4a4da4f7876bf64ad8fc0525242
VIPigeon <slv.bta@gmail.com>
2020-04-25 22:26:46 +0300

    !

---
+++ vk_api_test.py
@@ -0,0 +1,11 @@
+
+import vk_api
+
+
+def main():
+    token = 'b677113e34fc7a2aba2f407a091e4ff9e3a38d4c2e40f6945db05759646f9631f2c87ad072e250b462900'
+    vk_session = vk_api.VkApi(token=token)
+
+
+if __name__ == '__main__':
+    main()
